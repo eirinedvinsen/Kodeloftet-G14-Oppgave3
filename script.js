3b3207194268560543286721743dfbd2d23ccd91
eirinedvinsen <eirin.edvinsen@gmail.com>
2025-06-18 22:00:53 +0200

    Adding more parallax

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const planetIcon = document.querySelector("#planetIcon");
 console.log(planetIcon);
 
-planetIcon.addEventListener("click", function () {
+sunIcon.addEventListener("click", function () {
   const body = document.querySelector("body");
 
-  body.classList.remove("bgImg");
+  body.classList.remove("light");
   body.classList.add("dark");
 });
-
-hamburgerIcon.addEventListener("click", function () {
-    // Hente elementet som skal bli synlig
-    const navList = document.querySelector("#navList");
-    console.log(navList);
-  
-    navList.classList.remove("hidden");
-    navList.classList.add("navList");
-  });
